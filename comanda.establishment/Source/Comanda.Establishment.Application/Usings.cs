2af4c716c0ff2ab5cc2eaaf7103e70bc7148f61e
Richard Garcia <code.richardy@gmail.com>
2025-05-24 12:30:59 -0300

    feat: add global usings for System and Comanda namespaces

---
+++ comanda.establishment/Source/Comanda.Establishment.Application/Usings.cs
@@ -0,0 +1,14 @@
+/* global usings for System namespaces here */
+
+global using System.Reflection;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Text.Json.Serialization;
+
+/* global usings for Comanda namespaces here */
+
+global using Comanda.Establishment.Domain;
+global using Comanda.Establishment.Domain.Entities;
+
+/* global usings for third-party namespaces here */
+
+global using MediatR;
